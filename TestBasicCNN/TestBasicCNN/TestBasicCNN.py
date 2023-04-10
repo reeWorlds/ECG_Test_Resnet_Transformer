0a2c346e5c81d5abbb11561f33c79a0c652f3384
unknown <max.koval4uk@ukr.net>
2023-04-11 02:03:09 +0300

    added custom classes and prepared training

--- TestBasicCNN/TestBasicCNN/TestBasicCNN.py
+++ TestBasicCNN/TestBasicCNN/TestBasicCNN.py
@@ -4,6 +4,7 @@ from tensorflow.python.client import device_lib
 from tensorflow.keras.optimizers import schedules
 from tensorflow.keras.callbacks import ModelCheckpoint
 
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -43,7 +44,7 @@ def readTestData():
 	return testData, testMap, testRef
 
 
-def getScedule(initial_learning_rate, datasetLen, minibatch, epochN, num_waves=8, warmup_epochs=5):
+def getScedule(initial_learning_rate, datasetLen, minibatch, epochN, num_waves = 5, warmup_epochs = 8):
 	min_learning_rate = 2.5e-6
 
 	total_steps = (datasetLen + minibatch - 1) // minibatch * epochN
@@ -110,11 +111,63 @@ def plotTrainValidCurve(pathCSV, pathPNG):
 	plt.savefig(pathPNG)
 
 
-def getModel(lr_schedule, decay):
-	def weight_decay_schedule(step):
-		return lr_schedule(step) * decay
+class CustomLearningRateSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+	def __init__(self, initial_learning_rate, datasetLen, minibatch, epochN, num_waves=8, warmup_epochs=5):
+		super(CustomLearningRateSchedule, self).__init__()
+		self.initial_learning_rate = initial_learning_rate
+		self.datasetLen = datasetLen
+		self.minibatch = minibatch
+		self.epochN = epochN
+		self.num_waves = num_waves
+		self.warmup_epochs = warmup_epochs
+		self.lr_schedule = getScedule(initial_learning_rate, datasetLen, minibatch, epochN, num_waves, warmup_epochs)
 
-	opt = tfa.optimizers.AdamW(learning_rate=lr_schedule, weight_decay=weight_decay_schedule)
+	def __call__(self, step):
+		return self.lr_schedule(step)
+
+	def get_config(self):
+		return {
+			"initial_learning_rate": self.initial_learning_rate,
+			"datasetLen": self.datasetLen,
+			"minibatch": self.minibatch,
+			"epochN": self.epochN,
+			"num_waves": self.num_waves,
+			"warmup_epochs": self.warmup_epochs,
+		}
+
+
+class CustomAdamW(tfa.optimizers.AdamW):
+	def __init__(self, learning_rate, weight_decay_factor, **kwargs):
+		kwargs.pop("weight_decay", None)
+		super(CustomAdamW, self).__init__(learning_rate=learning_rate, weight_decay=0, **kwargs)
+		self.weight_decay_factor = weight_decay_factor
+
+	def _current_learning_rate(self, var_dtype):
+		step = tf.cast(self.iterations, var_dtype)
+		return self._get_hyper("learning_rate", var_dtype)(step)
+
+	def _resource_apply_dense(self, grad, var, apply_state=None):
+		weight_decay = self.weight_decay_factor * self._current_learning_rate(var.dtype)
+		if apply_state is None:
+			apply_state = {}
+		apply_state["weight_decay"] = weight_decay
+		return super(CustomAdamW, self)._resource_apply_dense(grad, var, apply_state=apply_state)
+
+	def _resource_apply_sparse(self, grad, var, indices, apply_state=None):
+		weight_decay = self.weight_decay_factor * self._current_learning_rate(var.dtype)
+		if apply_state is None:
+			apply_state = {}
+		apply_state["weight_decay"] = weight_decay
+		return super(CustomAdamW, self)._resource_apply_sparse(grad, var, indices, apply_state=apply_state)
+
+	def get_config(self):
+		config = super().get_config()
+		config["weight_decay_factor"] = self.weight_decay_factor
+		return config
+
+
+def getModel(custom_lr_schedule, decay):
+	opt = CustomAdamW(learning_rate=custom_lr_schedule, weight_decay_factor=decay)
 
 	model = keras.Sequential()
 	model.add(keras.layers.Conv1D(64, 192, kernel_initializer='normal', activation='relu', input_shape=(1500, 1)))
@@ -136,6 +189,7 @@ def getModel(lr_schedule, decay):
 	model.add(keras.layers.BatchNormalization())
 	model.add(keras.layers.Dense(3, kernel_initializer='normal', activation='softmax'))
 	model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+	
 	return model
 
 
@@ -145,17 +199,15 @@ def train(attempt, initial_learning_rate, weight_decay):
 
 	trainData, trainRef, validData, validRef = readData()
 
-	lr_schedule = getScedule(initial_learning_rate, len(trainData), batch_size, epochs)
-	model = getModel(lr_schedule, weight_decay)
+	custom_lr_schedule = CustomLearningRateSchedule(initial_learning_rate, len(trainData), batch_size, epochs)
+	model = getModel(custom_lr_schedule, weight_decay)
 
 	checkpoint_callback = ModelCheckpoint(filepath=f'../../models/BasicCNN/best_attempt{attempt}.h5', 
 									   monitor='val_accuracy', mode='max', save_best_only=True,
-									   save_weights_only=True, verbose=1)
+									   save_weights_only=False, verbose=1)
 
 	history = model.fit(trainData, trainRef, epochs=epochs, batch_size=batch_size,
 					 validation_data=(validData, validRef), verbose=1, callbacks=[checkpoint_callback])
-	
-	model.save(f'../../models/BasicCNN/final_attempt{attempt}.h5')
 
 	train_accuracy = history.history['accuracy']
 	val_accuracy = history.history['val_accuracy']
@@ -165,14 +217,64 @@ def train(attempt, initial_learning_rate, weight_decay):
 			f.write(f'{epoch},{train_acc},{val_acc}\n')
 
 
+def getF1Score(mat):
+	f1_scores = []
+	for i in range(3):
+		true_positive = mat[i, i]
+		false_positive = np.sum(mat[:, i]) - true_positive
+		false_negative = np.sum(mat[i, :]) - true_positive
+	
+		precision = true_positive / (true_positive + false_positive)
+		recall = true_positive / (true_positive + false_negative)
+	
+		f1_score = 2 * (precision * recall) / (precision + recall)
+		f1_scores.append(f1_score)
+
+	average_f1_score = np.mean(f1_scores)
+	return average_f1_score
+
+
+def getTestF1(attempt):
+	custom_objects = { "CustomAdamW": CustomAdamW, "CustomLearningRateSchedule": CustomLearningRateSchedule }
+	model = keras.models.load_model(f'../../models/BasicCNN/best_attempt{attempt}.h5', custom_objects=custom_objects)
+
+	testData, testMap, testRef = readTestData()
+
+	refMapping = {}
+	joined = {}
+
+	for index, row in testRef.iterrows():
+		refMapping[int(row[0])] = int(row[1])
+		joined[int(row[0])] = []
+
+	pred = model.predict(testData)
+	for i,preds in enumerate(pred):
+		joined[int(testMap[0][i])].append([-math.log(x) for x in list(preds)])
+	
+	mat = np.zeros((3,3))
+
+	for key in joined.keys():
+		predicted = np.sum(joined[key], axis=0).argmin()
+		gt = refMapping[key]
+		mat[gt,predicted] += 1
+
+	average_f1_score = getF1Score(mat)
+	
+	print(f"attempt {attempt} has f1 score {average_f1_score}")
+
+
 if __name__ == "__main__":
-	testGPU()
+	#testGPU()
 	
-	# plot_lr_schedule("../../models/BasicCNN/schedule.png")
+	#plot_lr_schedule("../../models/BasicCNN/schedule.png")
 	
-	train(1, 1e-3, 2.5e-4)
-	train(2, 1e-3, 2.5e-4)
-	train(3, 1e-3, 2.5e-4)
+	for attempt in range(1, 4):
+		train(attempt, 1e-3, 1e-1)
+		plotTrainValidCurve(f"../../models/basicCNN/history_attempt{attempt}.csv",
+					 f"../../models/basicCNN/history_attempt{attempt}.png")
 	
-	# plotTrainValidCurve("../../models/basicCNN/dummy.csv", "../../models/basicCNN/dummyAccuracy.png")
-	# todo get final accuracy on test set
+	for attempt in range(1, 4):
+		getTestF1(attempt)
+
+	# todo overnight training
+	# todo add logreg in training data
